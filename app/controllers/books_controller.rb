843ba07f6788fbf9d47859f66f809f7a5c9bf7d9
uedayusuke <uedayusuke1107@gmail.com>
2020-04-05 20:38:31 +0900

    [Add]edit destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,10 +17,26 @@ class BooksController < ApplicationController
   	@book = Book.new(book_params)
   	@book.user_id = current_user.id
   	@book.save
-  	redirect_to books_path #後で詳細ページに変更！
+  	redirect_to book_path(@book)
   end
 
   def edit
+    @book = Book.new
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
